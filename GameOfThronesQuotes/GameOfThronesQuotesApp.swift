00c54dcc3d6b9b95578881dcb04716de5a37b976
Steven Zhang <YZhang@lcs.on.ca>
2021-10-10 17:40:06 -0400

    Initial Commit

---
+++ GameOfThronesQuotes/GameOfThronesQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfThronesQuotesApp.swift
+//  GameOfThronesQuotes
+//
+//  Created by Steven Zhang on 2021-10-10.
+//
+
+import SwiftUI
+
+@main
+struct GameOfThronesQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
